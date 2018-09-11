d6a4d1a7488cf290ef17b0514effbdd0bce4217e
Jason Goodney <Jason.Goodney@gmail.com>
2018-09-11 10:28:24 -0600

    Initial Commit

---
+++ PokedexObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PokedexObjC
+//
+//  Created by Jason Goodney on 9/11/18.
+//  Copyright © 2018 Jason Goodney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PokedexObjC/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PokedexObjC
+//
+//  Created by Jason Goodney on 9/11/18.
+//  Copyright © 2018 Jason Goodney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
